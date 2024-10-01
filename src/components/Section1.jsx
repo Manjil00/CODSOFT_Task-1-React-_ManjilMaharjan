3557650d7e92b4d22964c61d3e61310c29ee0859
ACER <210220@softwarica.edu.np>
2024-10-01 18:01:18 +0545

    technical

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -10,7 +10,7 @@ return (
 
     <div className="innerContainer bg-bgcolorlite rounded-xl h-[600px] w-full relative flex flex-col md:flex-row  ">
         <div className="text flex flex-col justify-start items-start ml-8 md:ml-44">
-            <h1 className='text-2xl md:text-6xl font-bold text-start text-slate-500 mt-10'>Hello, I'm <span className='text-orange-500'>Manjil Maharjan</span>
+            <h1 className='text-2xl lg:text-6xl font-bold text-start text-slate-500 mt-10'>Hello, I'm <span className='text-orange-500'>Manjil Maharjan</span>
             <br/> And I'm a <span className='text-orange-500 '>Frontend Developer</span> </h1>
             <p className='mt-10 text-slate-500 '>Transforming complex ideas into simple and sophisticated web visuals, leveraging React.js,
                 <br/>seamless API integration, and intuitive UI design to create engaging and responsive user experiences. </p>
