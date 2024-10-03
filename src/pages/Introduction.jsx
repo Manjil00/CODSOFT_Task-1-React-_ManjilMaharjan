7ebc9fc61b793c3c606362c5bedd4fe26e3bb249
ACER <210220@softwarica.edu.np>
2024-10-03 22:12:08 +0545

    showcase

--- src/pages/Introduction.jsx
+++ src/pages/Introduction.jsx
@@ -41,7 +41,7 @@ return (
     <div className="innercontainer2 bg-bgcolorlite h-auto w-full rounded-xl flex flex-col justify-start items-start ">
     <h1 className="tech text-sm lg:text-4xl text-orange-500 mt-20 ml-10 font-bold ">TECHNICAL PROFICENCY</h1>
 
-    <div className="Progression bg-bgcolorlite h-auto w-full mt-10 md:mt-20 flex justify-start items-start gap-9 mx-2">
+    <div className="Progression h-auto w-full mt-10 md:mt-20 flex justify-start items-start gap-9 mx-2">
     <div className="UI/UX flex flex-col justify-between items-center md:p-2 md:mx-2 gap-16 md:gap-10 font-bold">
             <h1 className="UI/UX text-xs md:text-2xl text-slate-500">UI/UX Technologies <br/><span className='font-normal'>(Figma)</span></h1>
             <h1 className="frontend text-xs md:text-2xl text-slate-500">Frontend Technologies <br/><span className='font-normal'>(React.js-Redux, Next.js)</span></h1>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -8,20 +8,20 @@ import Footer from '../components/Footer';
 
 const Projects = () => {
 return (
-    <div className='mainContainer w-full h-full bg-bgcolor'>
+    <div className='mainContainer w-full h-auto bg-bgcolor'>
     <Nav/>
 
-    <div className="Section1 w-full h-[800px] bg-green-800 md:p-8 ">
-        <div className="innerContainer bg-bgcolorlite mt-5 md:mt-0 h-[800px] w-full rounded-xl py-8 ">
+    <div className="Section1 w-full h-auto bg-bgcolor md:p-8 ">
+        <div className="innerContainer bg-bgcolorlite mt-5 md:mt-0 h-auto w-full rounded-xl py-8 ">
             <h1 className="title text-orange-500 font-sans text-xl md:text-4xl font-bold ml-9 md:ml-14 ">PROJECT SHOWCASE</h1>
 
-            <div className="projects h-auto w-full flex flex-col justify-evenly items-center gap-5 mt-10 bg-yellow-500 p-2">
-                <div className="e-commerce bg-red-800 h-[200px] w-full flex flex-col md:flex-row justify-between items-center">
-                    <div className="words">
-                    <h1 className="title text-white ml-5 font-sans text-lg">Ecommerce website for Nursery firm.</h1>
-
+            <div className="projects h-auto w-full flex flex-col justify-evenly items-center gap-5 mt-5 md:mt-10 p-2">
+                <div className="project1 rounded-xl bg-bgcolor h-auto w-full flex flex-row justify-between items-start">
+                    <div className="words mt-3 md:mt-10">
+                    <h1 className="title text-white ml-5 font-sans text-lg md:text-2xl font-bold">Ecommerce website for Nursery Firm</h1>
+                    <p className=' text-white ml-5 font-sans text-xs md:text-lg mt-3'>Gajendra Nursery's e-commerce site, built with the MERN stack, features user authentication, a shopping cart,<br/>admin dashboard, and a responsive design for browsing and purchasing plants and services. It also includes <br/>a blog and supports gardening service bookings.</p>
                     </div>
-                    <div className="imgpart h-[200px] w-[200px] flex flex-col justify-center items-center">
+                    <div className="imgpart  rounded-tr-xl rounded-br-xl h-[200px] w-[200px] md:h-[500px] md:w-[500px] bg-white flex flex-col justify-center items-center">
                         <div className="upperimg flex">
                         <img src='' className='h-[25%] w-[25%]' alt='pic'/>
                         <img src='' className='h-[25%] w-[25%]' alt='pic'/>
@@ -30,6 +30,24 @@ return (
                     <img src='' className='h-[25%] w-[25%]' alt='pic'/>
                     <img src='' className='h-[25%] w-[25%]' alt='pic'/>
                     </div>
+                    </div>
+                </div>
+
+                <div className="project2 rounded-xl bg-bgcolor h-auto w-full flex flex-row justify-between items-start">
+                <div className="imgpart  rounded-tl-xl rounded-bl-xl h-[200px] w-[200px] md:h-[500px] md:w-[500px] bg-white flex flex-col justify-center items-center">
+                        <div className="upperimg flex">
+                        <img src='' className='h-[25%] w-[25%]' alt='pic'/>
+                        <img src='' className='h-[25%] w-[25%]' alt='pic'/>
+                        </div>
+                    <div className="part2img flex">
+                    <img src='' className='h-[25%] w-[25%]' alt='pic'/>
+                    <img src='' className='h-[25%] w-[25%]' alt='pic'/>
+                    </div>
+                    </div>
+
+                    <div className="words mt-3 md:mt-10">
+                    <h1 className="title text-white mr-5 font-sans text-lg md:text-2xl font-bold">Ecommerce website for Nursery Firm</h1>
+                    <p className=' text-white mr-5 font-sans text-xs md:text-lg mt-3'>Gajendra Nursery's e-commerce site, built with the MERN stack, features user authentication, a shopping cart,<br/>admin dashboard, and a responsive design for browsing and purchasing plants and services. It also includes <br/>a blog and supports gardening service bookings.</p>
                     </div>
                 </div>
 
