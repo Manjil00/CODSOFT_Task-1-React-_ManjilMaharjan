d26ff4f90311ac6c9e2b77d567fb0e0e881a5c35
ACER <210220@softwarica.edu.np>
2024-10-03 19:14:18 +0545

    project0

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -10,6 +10,33 @@ const Projects = () => {
 return (
     <div className='mainContainer w-full h-full bg-bgcolor'>
     <Nav/>
+
+    <div className="Section1 w-full h-[800px] bg-green-800 md:p-8 ">
+        <div className="innerContainer bg-bgcolorlite mt-5 md:mt-0 h-[800px] w-full rounded-xl py-8 ">
+            <h1 className="title text-orange-500 font-sans text-xl md:text-4xl font-bold ml-9 md:ml-14 ">PROJECT SHOWCASE</h1>
+
+            <div className="projects h-auto w-full flex flex-col justify-evenly items-center gap-5 mt-10 bg-yellow-500 p-2">
+                <div className="e-commerce bg-red-800 h-[200px] w-full flex flex-col md:flex-row justify-between items-center">
+                    <div className="words">
+                    <h1 className="title text-white ml-5 font-sans text-lg">Ecommerce website for Nursery firm.</h1>
+
+                    </div>
+                    <div className="imgpart h-[200px] w-[200px] flex flex-col justify-center items-center">
+                        <div className="upperimg flex">
+                        <img src='' className='h-[25%] w-[25%]' alt='pic'/>
+                        <img src='' className='h-[25%] w-[25%]' alt='pic'/>
+                        </div>
+                    <div className="part2img flex">
+                    <img src='' className='h-[25%] w-[25%]' alt='pic'/>
+                    <img src='' className='h-[25%] w-[25%]' alt='pic'/>
+                    </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
     <Footer/>
     </div>
 )
